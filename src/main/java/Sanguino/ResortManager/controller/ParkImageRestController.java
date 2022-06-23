3a229af3f4855108b29158084414d079501529f5
Alex <odross87@gmail.com>
2022-06-23 13:42:09 +0200

    Swagger with API Rest is working with image

--- src/main/java/Sanguino/ResortManager/controller/ParkImageRestController.java
+++ src/main/java/Sanguino/ResortManager/controller/ParkImageRestController.java
@@ -5,14 +5,12 @@ import Sanguino.ResortManager.model.ParkImage;
 import Sanguino.ResortManager.service.ParkImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("apiParkImage")
 public class ParkImageRestController {
 
     @Autowired
