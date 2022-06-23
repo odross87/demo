8d60e18746c9dd190e5dda99edec564c29a85f3b
Alex <odross87@gmail.com>
2022-06-23 18:05:23 +0200

    Images from Parks are working on View

--- src/main/java/Sanguino/ResortManager/controller/MyResortWebController.java
+++ src/main/java/Sanguino/ResortManager/controller/MyResortWebController.java
@@ -1,12 +1,15 @@
 package Sanguino.ResortManager.controller;
 
 
+import Sanguino.ResortManager.model.Park;
 import Sanguino.ResortManager.service.ParkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import Sanguino.ResortManager.service.AttractionService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/myresort")
@@ -23,6 +26,25 @@ public class MyResortWebController {
                 parkService.listOfParks());
         containerToView.addAttribute("attractionsfromController",
                 attractionService.ListAllAttractions());
+
+        return "web";
+    }
+
+    @RequestMapping("/detailPark")
+    public String getDetailPark (String id, Model containerToView) {
+        Optional<Park> parkToFound= parkService.findParkById(id);
+
+        if (parkToFound.isPresent()){
+            Park park = parkToFound.get();
+            containerToView.addAttribute("park",
+                    park);
+            containerToView.addAttribute("attractionsfromController",
+                    attractionService.ListAllAttractions());
+
+            return "parkDetail";
+        }
+
+
 
         return "web";
     }
